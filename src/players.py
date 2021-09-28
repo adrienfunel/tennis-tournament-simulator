956471c961ad9f7787ae6874aa604cf3ddf1cc43
Adrien Funel <funel.adrien@gmail.com>
2021-09-28 23:58:46 +0100

    Add a player class to generate participants

---
+++ src/players.py
@@ -0,0 +1,20 @@
+"""
+Author: Adrien Funel
+Date: Sept-2021
+Summary: create a player class with features
+"""
+
+class Player:
+
+    def __init__(self, id, power, technique, endurance):
+        self.id = id
+        self.power = power
+        self.technique = technique
+        self.endurance = endurance
+        self.status = None
+
+    def __str__(self):
+        return "Id is {};\n Power is {};\n Technique is {};\n Endurance is {}\n\n".format(self.id, self.power, self.technique, self.endurance)
+
+    def playerid(self):
+        return self.id
