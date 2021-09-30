b5ea65f29422431ca56d6f99d2dab588f614a898
Adrien Funel <funel.adrien@gmail.com>
2021-09-30 15:49:17 +0100

    Add a new method to play the games within the current round

--- src/brackets.py
+++ src/brackets.py
@@ -24,15 +24,19 @@ class Bracket:
         # return {'seed': i+1, 'p1': self.players_list[i], 'p2': self.players_list[i + 2], 'winner': '' for i in range(0, self.nb_players, 2)}
         for i, faceoff in enumerate(matchups):
             self.brackets.append(
-                {'seed':i+1, 'p1':faceoff[0], 'p2':faceoff[1], 'winner':None, 'score':''}
+                {'seed': i+1, 'p1': faceoff[0], 'p2': faceoff[1], 'winner': None, 'score': ''}
             )
-        return self.brackets
 
     def generate_next_round(self):
         prev_rounds = [self.brackets[i:i + 2] for i in range(0, len(self.brackets), 2)]
         self.brackets = []
-        for i, round in enumerate(prev_rounds):
+        for i, faceoff in enumerate(prev_rounds):
+            # print("The faceoff is {}".format(faceoff))
             self.brackets.append(
-                {'seed': i + 1, 'p1': round[0]['winner'], 'p2': round[1]['winner'], 'winner': None, 'score': ''}
+                {'seed': i + 1, 'p1': faceoff[0]['winner'], 'p2': faceoff[1]['winner'], 'winner': None, 'score': ''}
             )
-        return self.brackets
+
+    def play_games(self):
+        for game in self.brackets:
+            game.update({'seed': game['seed'], 'p1': game['p1'], 'p2': game['p2'], 'winner': game['p1'], 'score': '6-2'})
+
