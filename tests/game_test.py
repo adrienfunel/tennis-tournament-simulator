92a22a9c5bda9d3f0215b2d49d99ecb3c7d71050
Adrien Funel <funel.adrien@gmail.com>
2021-09-30 22:49:28 +0100

    Add unit testing for the game module

---
+++ tests/game_test.py
@@ -0,0 +1,40 @@
+"""
+Author: Adrien Funel
+Date: Sept-2021
+Summary: unit test for the game module
+"""
+import src.game as game
+from src.players import Player
+
+test_player1 = Player(1, 0.8, 0.8, 0.8)
+test_player2 = Player(1, 0.2, 0.2, 0.2)
+
+test_bracket = {'seed': 1,
+                'p1': test_player1,
+                'p2': test_player2,
+                'winner': None,
+                'score': ''}
+
+
+def test_play_match():
+    output = game.play_match(test_bracket)
+    print(output)
+
+
+def test_play_set():
+    output = game.play_set(test_player1, test_player2)
+    print(output)
+
+
+def test_play_game():
+    output = game.play_game(test_player1, test_player2)
+    print(output)
+
+
+def test_play_point():
+    output = game.play_point(test_player1, test_player2)
+    print(output)
+
+
+if __name__ == '__main__':
+    test_play_match()
