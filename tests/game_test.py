7da8cce385dd2efa393d7bdc9886b54d2ceb0f52
Adrien Funel <funel.adrien@gmail.com>
2021-10-06 12:06:37 +0100

    Add unit testing coverage for the game module

--- tests/game_test.py
+++ tests/game_test.py
@@ -14,10 +14,10 @@ test_player2 = Player(1, 0.2, 0.2, 0.2)
 test_player3 = Player(1, 0.8, 0.8, 0.8)
 
 test_bracket = {'seed': 1,
-                 'p1': test_player1,
-                 'p2': test_player2,
-                 'winner': None,
-                 'score': ''}
+                'p1': test_player1,
+                'p2': test_player2,
+                'winner': None,
+                'score': ''}
 
 
 class TestGame(unittest.TestCase):
@@ -27,10 +27,10 @@ class TestGame(unittest.TestCase):
         mocked_set.return_value = "set p1", "6-0"
 
         expected_bracket = test_bracket = {'seed': 1,
-                 'p1': test_player1,
-                 'p2': test_player2,
-                 'winner': test_player1,
-                 'score': '6-0;6-0;6-0'}
+                                           'p1': test_player1,
+                                           'p2': test_player2,
+                                           'winner': test_player1,
+                                           'score': '6-0;6-0;6-0'}
 
         input_expected = [
             (test_bracket, expected_bracket)
@@ -40,7 +40,6 @@ class TestGame(unittest.TestCase):
 
         self.assertEqual(result, expected_bracket)
 
-
     @mock.patch('src.game.play_game')
     def test_play_set(self, mocked_game):
         mocked_game.return_value = "game p1"
@@ -49,7 +48,6 @@ class TestGame(unittest.TestCase):
 
         self.assertEqual(result, ("set p1", "6-0"))
 
-
     @mock.patch('src.game.play_point')
     def test_play_game(self, mocked_point):
         mocked_point.return_value = "point p1"
@@ -57,6 +55,34 @@ class TestGame(unittest.TestCase):
         result = game.play_game(test_player1, test_player2)
 
         self.assertEqual(result, "game p1")
+
+    def test_play_point(self):
+        import random
+        random.seed(0)
+
+        input_expected = [
+            (test_player1, test_player2, "point p1"),
+            (test_player2, test_player1, "point p2")
+        ]
+
+        for input1, input2, output in input_expected:
+            self.assertEqual(game.play_point(input1, input2), output)
+
+    @mock.patch('src.game.play_point')
+    def test_play_deuce(self, mocked_point):
+        mocked_point.return_value = "point p1"
+
+        result = game.play_deuce(test_player1, test_player2)
+
+        self.assertEqual(result, "game p1")
+
+    @mock.patch('src.game.play_point')
+    def test_play_tiebreak(self, mocked_point):
+        mocked_point.return_value = "point p1"
+
+        result = game.play_tiebreak(test_player1, test_player2)
+
+        self.assertEqual(result, ("set p1", "7(7)-6(0)"))
 
 
 if __name__ == '__main__':
