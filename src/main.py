70591de82617617eea2c933c85192a79b7a71e87
Adrien Funel <funel.adrien@gmail.com>
2021-10-07 16:49:09 +0100

    Cap the number of player to 64, reformat linting

--- src/main.py
+++ src/main.py
@@ -14,16 +14,26 @@ logger = logging.getLogger()
 
 
 def setup():
+    """
+    Function to start service-level setup.
+    :return: N/A
+    """
     my_logger.set_logger()
 
 
 def main():
-    # main entry point
+    """
+    Main entry point.
+    :return: N/A
+    """
     nb_players = get_input_players()
-    while not math.log2(nb_players).is_integer():
-        print("The number of participants is invalid, "
+    while not math.log2(nb_players).is_integer() \
+            or not nb_players <= 64:
+        print("The number of participants is invalid,"
               "please enter a number of participants that fits a single-elimination tournament.\n"
-              "Nb: to be valid the number of participants must be a power of 2 (i.e 2, 4, 8, 16, etc.).")
+              "Nb: to be valid the number of participants must be a power of 2 (max 64)"
+              "(i.e 2, 4, 8, 16, etc.)."
+              )
         nb_players = get_input_players()
 
     logger.info("****** TENNIS TOURNAMENT SIMULATION STARTING ******")
@@ -32,11 +42,17 @@ def main():
 
 
 def get_input_players():
-    return int(input("Enter the number of participants:- "))
+    """
+    Function to get the number of players.
+    :return: int, inputed number of players
+    """
+    return int(input("Enter the number of participants (up to 64):- "))
 
 
 if __name__ == "__main__":
     start_time = time.time()
     setup()
     main()
-    logger.info("--- The service ran in %s seconds ---\n\n\n" % round((time.time() - start_time), 6))
+    logger.info("--- The service ran in %s seconds ---\n\n\n"
+                % round((time.time() - start_time), 6)
+                )
